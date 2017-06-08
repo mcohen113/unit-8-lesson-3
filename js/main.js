234a49ada5472b214a0fd1328608d77b79ffd5f9
mike cohen <mcohen113@msn.com>
2017-06-08 19:29:48 -0400

    stuck

--- js/main.js
+++ js/main.js
@@ -4,17 +4,33 @@
 
 
 // Step 2: When the user submits the form, do the following:
-	// a) Prevent the default action
-  // b) Create an object called userInput. There should be two key/value pairs in the object:
-  			// item: the value the user entered into the #item input (hint: use the val() method)
-  			// cost: the cost the user entered into the #cost input (hint: use the val() method)
+
+$('form').on('submit', function(event) {
+  event.preventDefault();
+
+
+  var userInput = {
+    item: $('#item').val()
+    cost: $('#cost').val()
+  }// b) Create an object called userInput. There should be two key/value pairs in the object:
+        // item: the value the user entered into the #item input (hint: use the val() method)
+        // cost: the cost the user entered into the #cost input (hint: use the val() method)
   // c) Get HTML from #groceryItem template and store it in a variable called source
-  var source = $("#entry-template").html();
+  var source = $("#groceryItem").html();
   // d) Use Handlebars.compile() to compile the source template and store in a variable called template.
   var template = Handlebars.compile(source);
   // e) Use template() to add the userInput to the template and store in a variable called newListItemHTML
-  var newListItemHTML = template += userInput;
+  var newListItemHTML = template(userInput);
   // f) Append newListItemHTML to the .groceries list
-  $(.groceries).append(newListItemHTML)
+  $('.groceries').append(newListItemHTML);
+});
 
 
+
+// var source = $("#entry-template").html();
+// var template = Handlebars.compile(source);
+// var data = {itemDescription: $('#newItemDescription').val() };
+// var newListItemHTML = template(data);
+
+// // Step 6: Append each new list item to the #packing list.
+// $('#packingList').append(newListItemHTML);
